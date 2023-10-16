a284ffbff48e5339d96059691fc2ab9cfdfa915d
vwaizer <lightwing2208@gmail.com>
2023-10-16 22:07:18 +0700

    add route

--- finalProject/src/App.js
+++ finalProject/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-
-
-
+import { BrowserRouter,Route } from "react-router-dom";
+import Home from './pages/Home';
+import Product from './pages/Product';
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/Product" element={<Product/>}></Route>
+    </BrowserRouter>
   );
 }
 
