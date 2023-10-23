5ee8cc85b126706409339bbac83378e8a3342314
dophambichtram <bichtramdopham@gmail.com>
2023-10-23 19:35:51 +0700

    up

--- finalProject/src/component/Header/Header.jsx
+++ finalProject/src/component/Header/Header.jsx
@@ -1,35 +1,33 @@
-import { ShoppingCartOutlined } from "@ant-design/icons";
-import React from "react";
-import Nav from "react-bootstrap/Nav";
-import "./header.style.css";
+import { ShoppingCartOutlined } from '@ant-design/icons';
+import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import './header.style.css';
 
 const infomation = [
   {
-    href: "/",
-    title: "Home",
+    href: '/',
+    title: 'Home',
   },
   {
-    href: "/product",
-    title: "Product",
+    href: '/product',
+    title: 'Product',
   },
   {
-    href: "/Cart",
-    title: "Cart and Payment",
+    href: '/Cart',
+    title: 'Cart and Payment',
   },
   {
-    href: "#",
-    title: "Contact",
+    href: '#',
+    title: 'Contact',
   },
 ];
 
-const form = [
-  { href: "/login", title: "Login" },
-];
+const form = [{ href: '/login', title: 'Login' }];
 function Header() {
   return (
     <div>
       <div>
-        <img src="" alt="#" />
+        <img src="logo.png" alt="#" />
       </div>
       <Nav className="header" as="ul">
         {infomation.map((item, index) => {
