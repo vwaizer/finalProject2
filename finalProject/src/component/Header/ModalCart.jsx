2382a0675877e030369adc56a8b6e190da871a7c
vwaizer <lightwing2208@gmail.com>
2023-10-31 20:47:52 +0700

    add couting

--- finalProject/src/component/Header/ModalCart.jsx
+++ finalProject/src/component/Header/ModalCart.jsx
@@ -1,26 +1,27 @@
-import React, { useState } from 'react';
-import { Button, Modal } from 'antd';
+import React, { useContext} from 'react';
+import { Avatar, Badge } from 'antd';
 import { ShoppingCartOutlined } from '@ant-design/icons';
 import './header.style.css';
+import { DataContext } from '../../App';
 const ModalCart = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const showModal = () => {
-    setIsModalOpen(true);
-  };
-  const handleOk = () => {
-    setIsModalOpen(false);
-  };
-  const handleCancel = () => {
-    setIsModalOpen(false);
-  };
+  let counting=0
+  const cartData=useContext(DataContext).data;
+  
+  cartData.forEach((item,index)=>{
+     counting++
+  })
+
   return (
     <>
-      <Button className='button-header' type="primary" onClick={showModal}>
+      {/* <Button className='button-header' type="primary" onClick={showModal}>
       <ShoppingCartOutlined/>
       </Button>
       <Modal title="Your Cart" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
         <p>Add content</p>
-      </Modal>
+      </Modal> */}
+       <Badge count={counting}>
+      <Avatar shape="square" size="middle" ><ShoppingCartOutlined/></Avatar>
+    </Badge>
     </>
   );
 };
