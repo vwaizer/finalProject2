b7d38c575b6cdc680afd60ff5fe23b7e80e209e8
NguyenVoHuyToan <huytoan9295@gmail.com>
2023-10-31 21:57:25 +0700

    update

--- finalProject/src/component/Header/ModalCart.jsx
+++ finalProject/src/component/Header/ModalCart.jsx
@@ -1,26 +1,54 @@
-import React, { useContext} from 'react';
-import { Avatar, Badge } from 'antd';
 import { ShoppingCartOutlined } from '@ant-design/icons';
-import './header.style.css';
+import { Badge, Button, Modal } from 'antd';
+import React, { useContext, useState } from 'react';
 import { DataContext } from '../../App';
+import './header.style.css';
+
+
 const ModalCart = () => {
   let counting=0
   const cartData=useContext(DataContext).data;
-  
+  console.log(cartData)
   cartData.forEach((item,index)=>{
      counting++
   })
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <>
-      {/* <Button className='button-header' type="primary" onClick={showModal}>
+       <Badge count={counting}>
+     <Button className='button-header' type="primary" onClick={showModal}>
       <ShoppingCartOutlined/>
       </Button>
       <Modal title="Your Cart" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-        <p>Add content</p>
-      </Modal> */}
-       <Badge count={counting}>
-      <Avatar shape="square" size="middle" ><ShoppingCartOutlined/></Avatar>
+        {cartData.map((item, index) => {
+            return(
+                <div className='Cart' key={item.id}>
+                    <div>
+                        <img className='img-cart' src={item.images} alt='#'></img>
+                    </div>
+                    <div>
+                        <li>{item.title}</li>
+                        <li>{item.price}</li>
+                    </div>
+                    <div>
+                        {item.amount}
+                    </div>
+                </div>
+            )
+        })}
+      </Modal> 
     </Badge>
     </>
   );
